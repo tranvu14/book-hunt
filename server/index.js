81f8efc62b2034c1059650cf703c23afe77e78a2
Vu Nguyen <tranvu.t1.bkdn@gmail.com>
2019-12-29 02:21:56 +0700

    setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const handle = require('./handlers')
+
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => res.json({
     hello: "world"
 }));
 
 app.use(handle.notFound);
-
 app.use(handle.errors);
 
 app.listen(port,console.log('Server started on port 4000'));
